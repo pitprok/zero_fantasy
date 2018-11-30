1c373369711cd322421fe063505c7f79baeb24de
pitprok <44905554+pitprok@users.noreply.github.com>
2018-11-30 22:45:44 +0200

    improved UI

--- WebContent/WEB-INF/jsp/test.jsp
+++ WebContent/WEB-INF/jsp/test.jsp
@@ -7,10 +7,86 @@
 <title>Alignment questionnaire</title>
 <style>
 #mainbackground {
-	background- background-size: cover;
+	background-size: cover;
 	background-repeat: no-repeat;
 	background-position: center center;
+	background: rgba(255, 255, 255, 0.5)
 }
+
+.myDiv {
+	position: relative;
+	z-index: 1;
+}
+
+.myDiv .bg {
+	position: absolute;
+	z-index: -1;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	background: url('${location.backgroundUrl}') center center;
+	opacity: .5;
+	width: 100%;
+	height: 100%;
+}
+
+#stats {
+	background: rgba(70, 72, 82, 0.5);
+	position: relative;
+}
+
+#statsTable {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+}
+
+#playerName {
+	position: absolute;
+	bottom: 55vh;
+	text-align: center;
+	font-size: calc(1vw + 1vh);
+}
+
+table.table-bordered>tbody>tr>th {
+	border: 3px solid #800000;
+	font-size: calc(1.1vw + 1.1vh)
+}
+
+table.table-bordered>tbody>tr>td {
+	border: 3px solid #800000;
+		font-weight: bold;
+	font-size: calc(1.1vw + 1.1vh)
+}
+
+table.table-bordered>tbody>tr>#hitpointsRow {
+	color: green;
+
+}
+
+table.table-bordered>tbody>tr>#attackRow {
+	color: red;
+
+}
+
+table.table-bordered>tbody>tr>#armorRow {
+	color: blue;
+
+}
+
+table.table-bordered>tbody>tr>#speedRow {
+	color: yellow;
+
+}
+
+table.table-bordered>tbody>tr>#accuracyRow {
+	color: silver;
+
+}
+
+
 </style>
 
 <link rel="stylesheet" href="resources/css/bootstrap.min.css">
@@ -27,6 +103,9 @@
 	<canvas id="bgCanvas"></canvas>
 	<canvas id="terCanvas"></canvas>
 
+
+
+
 	<div class="container-fluid .smooth-scroll">
 		<div class="row" style="height: 5vh">
 
@@ -34,44 +113,75 @@
 
 		</div>
 		<div class="row" style="height: 66vh">
-			<div class="col-2 bootstrapcolumn"></div>
+			<div class="col-2 bootstrapcolumn" id="stats">
+				<p id="playerName">
+					<strong>${player.username}</strong>
+				</p>
+
+				<table class="table table-bordered" id="statsTable">
+
+					<tbody>
+						<tr>
+							<th scope="row">Hitpoints:</th>
+							<td id="hitpointsRow">${player.hitpoints}</td>
+
+						</tr>
+						<tr>
+							<th scope="row">Attack:</th>
+							<td id="attackRow">${player.attack}</td>
+
+						</tr>
+						<tr>
+							<th scope="row">Armor:</th>
+							<td id="armorRow">${player.armor}</td>
+
+						</tr>
+						<tr>
+							<th scope="row">Speed:</th>
+							<td id="speedRow">${player.speed}</td>
+
+						</tr>
+						<tr>
+							<th scope="row">Accuracy:</th>
+							<td id="accuracyRow"}>${player.accuracy}</td>
+
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
 			<div class="col-8 bootstrapcolumn">
-				<div id="mainbackground"
-					style="background-image: url('${location.backgroundUrl}');">
-					<div class="someDivOverlay">
-						<div class="row " style="height: 45vh">
-
-							<div class="col-12 bootstrapmaincolumn wth centeredtext ">
-								<strong>${location.name}</strong><br>
-								<p id="maintext">${location.description}</p>
-							</div>
-
+				<div class="myDiv">
+					<div class="bg"></div>
+					<div class="row " style="height: 45vh">
+						<div class="col-12 bootstrapmaincolumn  centeredtext ">
+							<strong>${location.name}</strong><br>
+							<p id="maintext">${location.description}</p>
 						</div>
-						<div class="row" style="height: 15vh">
+					</div>
 
-							<div class="col-6 bootstrapmainbottomcolumnleft centeredtext ">
-								<p id="choices">${location.firstChoice}</p>
-							</div>
-
-
-							<div class="col-6 bootstrapmainbottomcolumnright centeredtext ">
-								<p id="choices">${location.secondChoice}</p>
-							</div>
+					<div class="row" style="height: 15vh">
+						<div class="col-6 bootstrapmainbottomcolumnleft centeredtext ">
+							<p id="choices">${location.firstChoice}</p>
 						</div>
-						<div class="row" style="height: 5vh">
-
-							<div class="col-6 bootstrapmainbottomcolumnleft centeredtext ">
-								<a href="${pageContext.request.contextPath}/advance?choice=A">
-									<input id="button" type="button" value="Choose" />
-								</a>
-							</div>
 
 
-							<div class="col-6 bootstrapmainbottomcolumnright centeredtext ">
-								<a href="${pageContext.request.contextPath}/advance?choice=B">
-									<input id="button" type="button" value="Choose" />
-								</a>
-							</div>
+						<div class="col-6 bootstrapmainbottomcolumnright centeredtext ">
+							<p id="choices">${location.secondChoice}</p>
+						</div>
+					</div>
+
+					<div class="row" style="height: 5vh">
+						<div class="col-6 bootstrapmainbottomcolumnleft centeredtext ">
+							<a href="${pageContext.request.contextPath}/advance?choice=A">
+								<input id="button" type="button" value="Choose" />
+							</a>
+						</div>
+
+						<div class="col-6 bootstrapmainbottomcolumnright centeredtext ">
+							<a href="${pageContext.request.contextPath}/advance?choice=B">
+								<input id="button" type="button" value="Choose" />
+							</a>
 						</div>
 					</div>
 				</div>
--- src/controllers/GeneralController.java
+++ src/controllers/GeneralController.java
@@ -35,9 +35,8 @@ public class GeneralController {
 
 	@RequestMapping("/")
 	public String index(HttpSession session) {
-		session.setAttribute("username", "tester");
+		session.setAttribute("username", "Argon the Barbarian");
 		session.setAttribute("currentLocation", "A");
-		System.out.println("ok");
 		return "forward:/game";
 	}
 
@@ -46,9 +45,12 @@ public class GeneralController {
 
 		List<Location> locationList = locationDAO.selectLocation((String) session.getAttribute("currentLocation"));
 		List<User> userList = userDAO.selectUser((String) session.getAttribute("username"));
+		
 		session.setAttribute("location", locationList.get(0));
 		session.setAttribute("player", userList.get(0));
+		
 		modelAndView.setViewName("test");
+		
 		return modelAndView;
 	}
 
@@ -56,66 +58,67 @@ public class GeneralController {
 	public ModelAndView advance(ModelAndView modelAndView, HttpSession session, String choice) {
 		String currentLocation = (String) session.getAttribute("currentLocation");
 		String nextLocation = currentLocation + choice;
+
 		if (choice.equals("A")) {
-			session.setAttribute("previousChoice",
-					((Location) session.getAttribute("location")).getFirstConsequenceText());
+			session.setAttribute("previousChoice",((Location) session.getAttribute("location")).getFirstConsequenceText());
 		} else if (choice.equals("B")) {
-			session.setAttribute("previousChoice",
-					((Location) session.getAttribute("location")).getSecondConsequenceText());
+			session.setAttribute("previousChoice",((Location) session.getAttribute("location")).getSecondConsequenceText());
 		}
+		
 		User user = (User) session.getAttribute("player");
 		// Edit user according to consequence
-		System.out.println(nextLocation	);
+		
 		List<Location> locationList = locationDAO.selectLocation(nextLocation);
 		List<User> userList = userDAO.selectUser((String) session.getAttribute("username"));
 
 		session.setAttribute("location", locationList.get(0));
 		session.setAttribute("player", userList.get(0));
 		session.setAttribute("currentLocation", nextLocation);
+		
 		modelAndView.setViewName("test");
 		return modelAndView;
 	}
 
-	@RequestMapping("/alignment")
-	public String alignment() {
-		return "alignment";
-	}
-
-	@RequestMapping(value = "/fastadvance", method = RequestMethod.GET, headers = "Accept=*/*", produces = "application/json")
-	public @ResponseBody String ajaxAdvance(String locationcode, HttpSession session) throws JsonProcessingException {
-		session.setAttribute("currentLocation", locationcode);
-		List<Location> locationList = locationDAO.selectLocation((String) session.getAttribute("currentLocation"));
-		ObjectMapper mapper = new ObjectMapper();
-		System.out.println(mapper.writeValueAsString(locationList.get(0)));
-		return mapper.writeValueAsString(locationList.get(0));
-	}
-
-	@RequestMapping(value = "/insert")
-	public ModelAndView insertUser(User player, ModelAndView modelAndView, HttpServletRequest request,
-			String password2) {
-//      (To be fixed)modelAndView.setView(new RedirectView(request.getContextPath()+"/jsp-name"));
-
-//		playerDAO.insert(player);
-
-		return modelAndView;
-	}
-
-	@RequestMapping(value = "/loginform")
-	public ModelAndView userLogin(ModelAndView modelAndView) {
-//      (To be fixed)modelAndView.setViewName("jsp-name");
-		User player = new User();
-		modelAndView.addObject("user", player);
-
-		return modelAndView;
-	}
-
-	@RequestMapping(value = "/registrationform")
-	public ModelAndView insertUserForm(ModelAndView modelAndView) {
-		User player = new User();
-//      (To be fixed)modelAndView.setViewName("registration-jsp-name");
-		modelAndView.addObject("user", player);
-
-		return modelAndView;
-	}
+//	@RequestMapping("/alignment")
+//	public String alignment() {
+//		return "alignment";
+//	}
+//
+//	@RequestMapping(value = "/fastadvance", method = RequestMethod.GET, headers = "Accept=*/*", produces = "application/json")
+//	public @ResponseBody String ajaxAdvance(String locationcode, HttpSession session) throws JsonProcessingException {
+//		session.setAttribute("currentLocation", locationcode);
+//		List<Location> locationList = locationDAO.selectLocation((String) session.getAttribute("currentLocation"));
+//		ObjectMapper mapper = new ObjectMapper();
+//		System.out.println(mapper.writeValueAsString(locationList.get(0)));
+//		return mapper.writeValueAsString(locationList.get(0));
+//	}
+//
+//	@RequestMapping(value = "/insert")
+//	public ModelAndView insertUser(User player, ModelAndView modelAndView, HttpServletRequest request,
+//			String password2) {
+////      (To be fixed)modelAndView.setView(new RedirectView(request.getContextPath()+"/jsp-name"));
+//
+////		playerDAO.insert(player);
+//
+//		return modelAndView;
+//	}
+//
+//	@RequestMapping(value = "/loginform")
+//	public ModelAndView userLogin(ModelAndView modelAndView) {
+////      (To be fixed)modelAndView.setViewName("jsp-name");
+//		User player = new User();
+//		modelAndView.addObject("user", player);
+//
+//		return modelAndView;
+//	}
+//
+//	@RequestMapping(value = "/registrationform")
+//	public ModelAndView insertUserForm(ModelAndView modelAndView) {
+//		User player = new User();
+////      (To be fixed)modelAndView.setViewName("registration-jsp-name");
+//		modelAndView.addObject("user", player);
+//
+//		return modelAndView;
+//	}
 
 }
